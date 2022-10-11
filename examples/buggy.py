0a0fe60779f6362b7ec767a85904e857b6852812
Corentin Cadiou <corentin.cadiou@cphyc.me>
2022-10-11 09:47:35 +0200

    Initial commit of file. This is now adding the sums

---
+++ examples/buggy.py
@@ -0,0 +1,18 @@
+"Example of a file that introduced a bug... at some point!"
+
+my_csv = """1,2,3
+4,5,6
+7,8,9
+10,11,12"""
+
+# Extract each value in the csv and unpack a list of lists
+result = []
+for line in my_csv.splitlines():
+    values_as_str = line.split(",")
+    values_as_int = [int(value) for value in values_as_str]
+
+    result.append(values_as_int)
+
+# Now print the sum of each column
+for i, column in enumerate(zip(*result)):
+    print(f"Column {i}: sum={sum(column)}")

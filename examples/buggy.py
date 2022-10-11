f5b1c8c8b909e54b97c1d24f5c0d778f648c3d58
Corentin Cadiou <corentin.cadiou@cphyc.me>
2022-10-11 09:49:12 +0200

    Wrap everything in a main function to abide to standards

--- examples/buggy.py
+++ examples/buggy.py
@@ -1,18 +1,22 @@
 "Example of a file that introduced a bug... at some point!"
 
-my_csv = """1,2,3
-4,5,6
-7,8,9
-10,11,12"""
+def main():
+    my_csv = """1,2,3
+    4,5,6
+    7,8,9
+    10,11,12"""
 
-# Extract each value in the csv and unpack a list of lists
-result = []
-for line in my_csv.splitlines():
-    values_as_str = line.split(",")
-    values_as_int = [int(value, 2) for value in values_as_str]
+    # Extract each value in the csv and unpack a list of lists
+    result = []
+    for line in my_csv.splitlines():
+        values_as_str = line.split(",")
+        values_as_int = [int(value, 2) for value in values_as_str]
 
-    result.append(values_as_int)
+        result.append(values_as_int)
 
-# Now print the sum of each column
-for i, column in enumerate(zip(*result)):
-    print(f"Column {i}: sum={sum(column)}")
+    # Now print the sum of each column
+    for i, column in enumerate(zip(*result)):
+        print(f"Column {i}: sum={sum(column)}")
+
+if __name__ == "__main__":
+    main()
